e4f856ceaacbd11fd37d6058c22dd3670a5bcd23
woojin7.ahn <woojin7.ahn@samsung.com>
2015-11-02 21:31:52 +0900

    code 8-3 send() method

---
+++ code-8.2.js
@@ -0,0 +1,30 @@
+/**
+ * Created by woojin on 2015-11-02.
+ */
+var http = require('http');
+var express = require('express');
+
+// 서버 생성
+var app = express();
+
+// request 이벤트 리스너 설정
+app.use(function (request, response) {
+    // 데이터 생성
+    var output = [];
+    for (var i = 0; i < 3; i++) {
+        output.push({
+            count: i,
+            name: 'name-'+i
+        })
+    }
+
+    // 응답
+    response.send(output);
+    /*response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end('<h1>Hello express!</h1>');*/
+});
+
+// 서버실행
+http.createServer(app).listen(52273, function () {
+    console.log('서버가 시작되었습니다. http://127.0.0.1:52273');
+});
